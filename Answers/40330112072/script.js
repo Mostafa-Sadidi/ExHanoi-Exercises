c2635927e0a1b3b440b5368c753a23604f2f3eea
Mostafa-Sadidi <mosisadidi2018@gmail.com>
2025-11-01 23:55:59 +0330

    Exhanoi 1-4 fixed.

--- Answers/40330112072/script.js
+++ Answers/40330112072/script.js
@@ -53,15 +53,14 @@ function hanoi(from, via, to, n) {
 
 function exHanoi_1(start, aux, end, n) {
     if(n == 1){
-        hanoi(end, start, aux, 3);
-        moves.push([start, end]);
-        hanoi(aux, start, end, 5);
+        moves.push([start, aux]);
+        hanoi(aux, start, end, 3);
     }
     else{
         exHanoi_1(start, aux, end, n-1);
-        hanoi(end, aux, start, 6*n - 3);
-        moves.push([start, end]);
-        hanoi(aux, start, end, 6*n - 1);
+        moves.push([start, aux]);
+        hanoi(end, start, aux, 6*n - 6);
+        hanoi(aux, start, end, 6*n - 3);
     }
 }  
 
@@ -90,9 +89,11 @@ function exhanoi_4(A, B, C, D, n) {
         moves.push([A, C]);
     else{
         exhanoi_4(A, B, C, D, n-1);
-        moves.push([A, C]);
-        exhanoi_4(D, B, C, A, n-1);
-        exhanoi_4(A, B, D, C, n-1);
+        moves.push([A, D]);
+        exhanoi_4(C, D, A, B, n-1);
+        moves.push([D, B]);
+        moves.push([B, C]);
+        exhanoi_4(A, B, C, D, n-1);
     }
 }
 
